fdf26613388ec257f186f6aa2bbfdbc449595644
Fabián Delgado <fabian.delgado@outlook.com>
2015-09-17 13:09:33 -0300

    Se cambian migración powers

--- db/migrate/20150816215114_create_powers.rb
+++ db/migrate/20150816215114_create_powers.rb
@@ -2,8 +2,8 @@ class CreatePowers < ActiveRecord::Migration
   def change
     create_table :powers do |t|
       t.integer :day
-      t.string :start
-      t.string :end
+      t.string :on
+      t.string :off
       t.integer :emission_id
       t.timestamps :timestamp
     end
