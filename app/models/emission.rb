91058571e23805dd96c2763bdb2b9c0b3ed08098
Fabián Delgado <fabian.delgado@outlook.com>
2015-09-17 12:50:50 -0300

    Se agrega una llamada a la función que genera la llave en el modelo

--- app/models/emission.rb
+++ app/models/emission.rb
@@ -2,10 +2,12 @@ class Emission < ActiveRecord::Base
 	has_and_belongs_to_many :users
 	belongs_to :trade
 	has_many :upload, :dependent => :destroy
-  has_one :machine, :dependent => :destroy
-  has_one :power, :dependent => :destroy
-  has_many :statistic, :dependent => :destroy
+  	has_one :machine, :dependent => :destroy
+  	has_one :power, :dependent => :destroy
+  	has_many :statistic, :dependent => :destroy
 
+  	#Antes de guardar llama a la función que genera la llave
+  	before_save :converted_to_key
 	#Permite serializar los multiples chechboxes de la opciones news_sources
 	serialize :news_source
 
