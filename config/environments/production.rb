95838e463e7fcfa048aecd300ba39867b92bd2d1
Fabián Delgado <fabian.delgado@outlook.com>
2015-09-21 11:39:57 -0300

    Cambios en configuración para rails 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   #Server files compiled in production
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
